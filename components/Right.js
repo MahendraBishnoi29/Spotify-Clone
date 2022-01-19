7ce80aa609b8f059ae3b8a94d4130a60d9c98f0f
Mahendra <mahendrabrowny@gmail.com>
2022-01-19 17:52:20 +0530

    Right Section is done and Login Logout is also completed

--- components/Right.js
+++ components/Right.js
@@ -1,8 +1,25 @@
+import { HiOutlineShieldCheck } from "react-icons/hi";
+import { MdOutlineSettings } from "react-icons/md";
+import { BiBell } from "react-icons/bi";
+import {} from "@heroicons/react/solid";
+import Dropdown from "./Dropdown";
+
 function Right() {
   return (
-    <div>
-      <h1>Right</h1>
-    </div>
+    <section className="p-4 space-y-8 pr-8">
+      <div className="flex space-x-2 items-center justify-between">
+        {/* Icons */}
+        <div className="flex items-center space-x-4 border-2 border-[#262626] rounded-full h-12 py-3 px-4">
+          <HiOutlineShieldCheck className="text-[#CCCCCC] text-xl" />
+          <MdOutlineSettings className="text-[#CCCCCC] text-xl" />
+          <div>
+            <BiBell className="text-[#CCCCCC] text-xl" />
+          </div>
+        </div>
+        {/* Profile */}
+        <Dropdown />
+      </div>
+    </section>
   );
 }
 
